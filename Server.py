41f64ade255f396eae9457fa563998bfcbf1841d
leafz <zlf123@mail.ustc.edu.cn>
2018-06-16 15:59:46 +0800

    add 'Open' in server

--- Server.py
+++ Server.py
@@ -41,6 +41,7 @@ class Server:
                 nodes = Node(name=op[2], path=op[1], ext=op[3], uid=op[4], atime=op[5], mtime=op[6], ctime=op[7])
                 filename = op[2].split('.')
                 filename = filename[0]
+                print('filename: ', filename)
                 labels = gp.getproperty(filename)
                 self.graph.create(nodes)
                 if labels[0] == 'OK':
@@ -66,28 +67,33 @@ class Server:
         elif op[0] == 'Open':
             nodes = self.matchper('path', op[1])
             opentime = time.mktime(time.strptime(op[2], '%a %b %d %H:%M:%S %Y'))
+            print('filedict: ', filedict)
             if abs(filedict['time'] - opentime) < 100:
                 if filedict['file1'] == op[1] or filedict['file2'] == op[1] or filedict['file3'] == op[1]:
                     lastfile = filedict['initfile']
                     node1 = self.matchper('path', lastfile)
                     node1 = node1[0]
                     node2 = nodes[0]
-                    result = self.relmatcher(node1, node2)
-                    rel = result[0]
-                    if rel['weight'] < 10:
-                        rel['weight'] += 1
-                        self.graph.push(rel)
+                    rels = self.relmatcher(node1, node2)
+                    for rel in rels:
+                        if rel['weight'] < 10:
+                            rel['weight'] += 1
+                            self.graph.push(rel)
                     for i in range(3):
-                        if filedict['file' + str(i + 1)] == op[1]:
+                        if filedict['file' + str(i + 1)] == op[1] or filedict['file' + str(i + 1)] == '':
                             continue
                         else:
                             node3 = self.matchper('path', filedict['file' + str(i + 1)])
+                            if node3 == []:
+                                print('Wrong input')
+                                continue
                             node3 = node3[0]
-                            rel = self.relmatcher(node1, node3)
-                            rel = rel[0]
-                            if rel['weight'] >= 6:
-                                rel['weight'] -= 1
-                                self.graph.push(rel)
+                            rels = self.relmatcher(node1, node3)
+                            print(rels)
+                            for rel in rels:
+                                if rel['weight'] >= 6:
+                                    rel['weight'] -= 1
+                                    self.graph.push(rel)
             node = nodes[0]
             node['atime'] = op[2]
             filedict['initfile'] = op[1]
@@ -99,12 +105,18 @@ class Server:
 
         elif op[0] == 'Read':
             nodes = self.matchper('path', op[1])
+            if nodes == []:
+                print('Wrong op')
+                return
             node = nodes[0]
             node['atime'] = op[2]
             self.graph.push(node)
 
         elif op[0] == 'Rename':
             nodes = self.matchper('path', op[1])
+            if nodes == []:
+                print('Wrong op')
+                return
             node = nodes[0]
             node['path'] = op[2]
             node['name'] = op[3]
@@ -113,13 +125,28 @@ class Server:
             self.graph.push(node)
 
         elif op[0] == 'Unlink':
+            if filedict['initfile'] == op[1]:
+                filedict['initfile'] = ''
+                for j in range(3):
+                    filedict['file' + str(j + 1)] = ''
+                filedict['time'] = 0.0
+            for j in range(3):
+                if filedict['file' + str(j + 1)] == op[1]:
+                    filedict['file' + str(j + 1)] = ''
+
             nodes = self.matchper('path', op[1])
+            if nodes == []:
+                print('Wrong op')
+                return
             node = nodes[0]
             print(node)
             self.graph.delete(node)
 
         elif op[0] == 'Chown':
             nodes = self.matchper('path', op[1])
+            if nodes == []:
+                print('Wrong op')
+                return
             node = nodes[0]
             node['uid'] = op[2]
             node['ctime'] = op[3]
@@ -127,6 +154,9 @@ class Server:
         
         elif op[0] == 'Change':
             nodes = self.matchper('path', op[1])
+            if nodes == []:
+                print('Wrong op')
+                return
             node = nodes[0]
             node['atime'] = op[2]
             node['mtime'] = op[3]
@@ -154,7 +184,7 @@ if __name__ == '__main__':
     print('Server start at: %s:' %HOST)
     print('wait for connection...')
     
-    filedict = {'initfile':'', 'file1':'', 'file2':'', 'file3':'', 'time':''}
+    filedict = {'initfile':'', 'file1':'', 'file2':'', 'file3':'', 'time':1529125142.0}
 
     while True:
         conn, addr = s.accept()
--- client.py
+++ client.py
@@ -6,7 +6,9 @@ s.connect(HOST)
 
 while True:
     cmd = input("Please input msg:")
-    s.send(('0,Create,/aaa/bbb/pathaaaTEST,操作系统.ttt,.ttt,leafz,1:23,1:25,1:24').encode('UTF-8'))
+    s.send(('0,Open,/aaa/bbb/pathaaa,Sat Jun 16 12:59:02 2018,/aaa/bbb/path').encode('UTF-8'))
+    #s.send(('0,Open,/aaa/bbb/1112222,Sat Jun 16 12:59:02 2018,/aaa/bbb/pathaaa').encode('UTF-8'))  
+    #s.send(('0,Create,/aaa/bbb/1112222,计算机.ppt,.ttt,leafz,1:3,1:25,1:24').encode('UTF-8'))
     #s.send(b'0,Unlink,/aaa/bbb/path')
     data = s.recv(1024)
     print(data)
